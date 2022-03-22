482cc786f428f8455b6c2c32a8ee7bcceb947bf9
Ty Vile <ty.jvile@gmail.com>
2022-03-23 01:12:26 +1030

    auth

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,4 @@ app.get("/", (req, res) => {
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+app.listen(PORT, () => console.log(PORT));
